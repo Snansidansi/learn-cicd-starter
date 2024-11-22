c856cf11aad04d06b4d176614d7d70495e6fe928
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2024-11-22 09:13:41 +0100

    Added unit test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+    "net/http"
+    "testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	t.Run("Valid Authorization Header", func(t *testing.T) {
+		headers := http.Header{}
+		headers.Set("Authorization", "ApiKey valida pikey123")
+
+		apiKey, err := GetAPIKey(headers)
+
+		if err != nil || apiKey != "validapikey123" {
+			t.Errorf("Expected apiKey: validapikey123, got: %s, error: %v", apiKey, err)
+		}
+	})
+}
+
